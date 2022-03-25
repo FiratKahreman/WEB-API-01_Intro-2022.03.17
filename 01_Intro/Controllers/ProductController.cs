27b3c924d2c7b1fb1b4180fb81c201bd4e644f67
Fırat Kahreman <firatkahrmn@gmail.com>
2022-03-25 23:54:09 +0300

    Add project files.

---
+++ 01_Intro/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01_Intro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ 01_Intro/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// Reflection
+
+namespace _01_Intro.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController()
+        {
+            //ValuesController vc = new ValuesController();
+            //vc.GetValues();
+        }
+
+        // Http Method / Verb
+        public IActionResult GetValues()
+        {
+            List<string> cities = new List<string>() { "İstanbul", "Ankara", "İzmir" };
+            return Ok(cities);
+        }
+
+        public IActionResult GetCities()
+        {
+            List<string> cities = new List<string>() { "İstanbul", "Ankara", "İzmir" };
+            return Ok(cities);
+        }
+    }
+}
